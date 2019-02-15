4ab6980ff5371e29365bbdbdc24a8258277b9c93
aapokaapo <47606265+aapokaapo@users.noreply.github.com>
2019-02-15 11:28:28 +0200

    Update rankingsystem.py

--- rankingsystem.py
+++ rankingsystem.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from operator import attrgetter
 from dplib.server import Server
 import numpy as np
 
