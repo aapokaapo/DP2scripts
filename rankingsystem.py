b248b34065cba356a2b4189978ca31358dbd40ab
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2019-02-19 20:58:29 +0200

    PEP8

--- rankingsystem.py
+++ rankingsystem.py
@@ -14,17 +14,19 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#Ranking system
+# Ranking system
 
 from dplib.server import Server
 
-s = Server(hostname='178.157.90.120', port=44444, logfile=r'/home/paintball/paintball2/pball/qconsole44444.log', rcon_password='endless')
-
+s = Server(hostname='178.157.90.120', port=44444,
+           logfile=r'/home/paintball/paintball2/pball/qconsole44444.log',
+           rcon_password='endless')
 
 
 class Player_stats():
-    
-    def __init__(self, name, id, kills=0, deaths=0, caps=0, grabs=0, pgp=0, score=0):
+
+    def __init__(self, name, id, kills=0, deaths=0, caps=0, grabs=0, pgp=0,
+                 score=0):
         self.name = name
         self.id = id
         self.kills = kills
@@ -33,12 +35,12 @@ class Player_stats():
         self.grabs = grabs
         self.pgp = pgp
         self.score = score
-    
+
     def add_kill(self, kills=1, score=1):
         print('Player ' + self.name + ' got a point')
         self.kills += kills
         self.score += score
-        
+
     def add_capture(self, caps=1, score=5):
         print('Player ' + self.name + ' got 5 points')
         self.caps += caps
@@ -48,19 +50,23 @@ class Player_stats():
         print('Player ' + self.name + ' lost a point')
         self.deaths += deaths
         self.score -= score
-        
+
     def add_pgp(self, pgp=1):
         print('Player ' + self.name + ' got a pgp kill')
         self.pgp += pgp
-        
+
     def add_grab(self, grabs=1, score=3):
         print('Player ' + self.name + ' got a grab')
         self.grabs += grabs
         self.score += score
- 
+
+
 list = [Player_stats('DPBot01', 'bot')]
-#@s.event
-#def on_entrance(nick, build, addr):
+
+
+# @s.event
+# def on_entrance(nick, build, addr):
+#    return
 #    players = s.get_players()
 #    for player in players:
 #        if player.nick == nick:
@@ -71,51 +77,67 @@ list = [Player_stats('DPBot01', 'bot')]
 #        if entered_player.dplogin == player_stats.id:
 #            player_found = True
 #            player_stats.name = nick
-#            print ('Player ' + player_stats.name + ' logged in')
+#            print('Player ' + player_stats.name + ' logged in')
 #            print(str(list))
-#            s.say ("{}: K{}/D{}/C{}/S{}".format(player_stats.name, player_stats.kills, player_stats.deaths, scoreboard.caps, scoreboard.score))
+#            s.say("{}: K{}/D{}/C{}/S{}".format(player_stats.name,
+#                                               player_stats.kills,
+#                                               player_stats.deaths,
+#                                               player_stats.caps,
+#                                               player_stats.score))
 #    if not player_found:
 #        list.append(Player_stats(nick, entered_player.dplogin))
-#        print('Player ' +  nick + ' added to list.')
+#        print('Player ' + nick + ' added to list.')
 #        print(str(list))
-        
-def get_rank(nick):        
-    print ('Player ' + nick + ' requested scoreboard')
+
+
+def get_rank(nick):
+    print('Player ' + nick + ' requested scoreboard')
     for i in range(len(list)):
         player_stats = list[i]
         if player_stats.name == nick:
-            s.say ("{}: K{}/D{}/C{}/G{}/S{}".format(player_stats.name, player_stats.kills, player_stats.deaths, player_stats.caps, player_stats.grabs, player_stats.score))
+            s.say("{}: K{}/D{}/C{}/G{}/S{}".format(player_stats.name,
+                                                   player_stats.kills,
+                                                   player_stats.deaths,
+                                                   player_stats.caps,
+                                                   player_stats.grabs,
+                                                   player_stats.score))
             break
         else:
-            s.say ("{0} not logged in".format(nick))
+            s.say("{0} not logged in".format(nick))
             break
-            
-def get_pgprank(nick):        
-    print ('Player ' + nick + ' requested PGP-scores')
+
+
+def get_pgprank(nick):
+    print('Player ' + nick + ' requested PGP-scores')
     for player_stats in list:
         if nick == player_stats.name:
-            s.say ("{}: PGP:{}".format(player_stats.name, player_stats.pgp))
+            s.say("{}: PGP:{}".format(player_stats.name, player_stats.pgp))
             break
         else:
-            s.say ("{0} not logged in".format(nick))
+            s.say("{0} not logged in".format(nick))
+
 
 def get_top10():
     list.sort(reverse=True, key=lambda player_stats: player_stats.score)
     try:
         for i in range(10):
             player_stats = list[i]
-            s.say("#{}:{}: Score:{}".format(i, player_stats.name, player_stats.score))
+            s.say("#{}:{}: Score:{}".format(
+                i, player_stats.name, player_stats.score))
     except IndexError:
         print("Less than 10 players on list")
+
 
 def get_pgptop10():
     list.sort(reverse=True, key=lambda player_stats: player_stats.pgp)
     try:
         for i in range(10):
             player_stats = list[i]
-            s.say("#{}:{}: Score:{}".format(i, player_stats.name, player_stats.score))
+            s.say("#{}:{}: Score:{}".format(
+                i, player_stats.name, player_stats.score))
     except IndexError:
         print("Less than 10 players on list")
+
 
 def add_player(nick):
     players = s.get_players()
@@ -132,32 +154,34 @@ def add_player(nick):
         player_stats = list[i]
         print("Comparing dplogin id to player_stats.id")
         print("List:#{}:{}:{}".format(i, player_stats.name, player_stats.id))
-        print("Trying to add:{}:{}".format(entered_player.nick, entered_player.dplogin))
+        print("Trying to add:{}:{}".format(
+            entered_player.nick, entered_player.dplogin))
         if player_stats.id == entered_player.dplogin:
-            print("Player {}:{} found from list!".format(nick, entered_player.dplogin))
+            print("Player {}:{} found from list!".format(
+                nick, entered_player.dplogin))
             player_stats.name = nick
         player_found = True
         if not player_found:
             list.append(Player_stats(nick, entered_player.dplogin))
-            print("Player {}:{} added to list".format(nick, entered_player.dplogin))
+            print("Player {}:{} added to list".format(
+                nick, entered_player.dplogin))
 
 
-            
 @s.event
 def on_chat(nick, message):
     if message == '!rank':
         get_rank(nick)
-               
+
     if message == '!top10':
         get_top10()
-     
+
     if message == '!addplayer':
-        print("trying to add player")        
+        print("trying to add player")
         add_player(nick)
-    
+
     if message == '!pgp':
         get_pgprank(nick)
-    
+
     if message == '!pgptop10':
         get_pgptop10()
 
@@ -169,6 +193,7 @@ def on_flag_captured(team, nick, flag):
             player_stats.add_capture()
             break
 
+
 @s.event
 def on_elim(killer_nick, killer_weapon, victim_nick, victim_weapon):
     print("eliminated")
@@ -179,5 +204,6 @@ def on_elim(killer_nick, killer_weapon, victim_nick, victim_weapon):
             player_stats.add_pgp(killer_nick)
         if victim_nick == player_stats.name:
             player_stats.add_death()
+
 
 s.run()
