684d76e65e98724319238703582f560f737cebf6
aapokaapo <47606265+aapokaapo@users.noreply.github.com>
2019-02-19 14:39:33 +0200

    Update rankingsystem.py

--- rankingsystem.py
+++ rankingsystem.py
@@ -24,29 +24,39 @@ s = Server(hostname='178.157.90.120', port=44444, logfile=r'/home/paintball/pain
 
 class Player_stats():
     
-    def __init__(self, name, id, kills=0, deaths=0, caps=0, pgp=0, score=0):
+    def __init__(self, name, id, kills=0, deaths=0, caps=0, grabs=0, pgp=0, score=0):
         self.name = name
         self.id = id
         self.kills = kills
         self.deaths = deaths
         self.caps = caps
+        self.grabs = grabs
         self.pgp = pgp
         self.score = score
     
     def add_kill(self, kills=1, score=1):
-        print ('Player ' + self.name + ' got a point')
+        print('Player ' + self.name + ' got a point')
         self.kills += kills
         self.score += score
         
     def add_capture(self, caps=1, score=5):
-        print ('Player ' + self.name + ' got 5 points')
+        print('Player ' + self.name + ' got 5 points')
         self.caps += caps
         self.score += score
 
     def add_death(self, deaths=1, score=1):
-        print ('Player ' + self.name + ' lost a point')
+        print('Player ' + self.name + ' lost a point')
         self.deaths += deaths
         self.score -= score
+        
+    def add_pgp(self, pgp=1):
+        print('Player ' + self.name + ' got a pgp kill')
+        self.pgp += pgp
+        
+    def add_grab(self, grabs=1, score=3)
+        print('Player ' + self.name + ' got a grab')
+        self.grabs += grabs
+        self.score += score
  
 list = [Player_stats('DPBot01', 'bot')]
 #@s.event
@@ -74,7 +84,18 @@ def get_rank(nick):
     for player_stats in list:
     #tämä ei käy kaikki player_stats objecteja läpi listasta list
         if nick == player_stats.name:
-            s.say ("{}: K{}/D{}/C{}/S{}".format(player_stats.name, player_stats.kills, player_stats.deaths, player_stats.caps, player_stats.score))
+            s.say ("{}: K{}/D{}/C{}/G{}/S{}".format(player_stats.name, player_stats.kills, player_stats.deaths, player_stats.caps, player_stats.grabs, player_stats.score))
+            break
+        else:
+            s.say ("{0} not logged in".format(nick))
+            break
+            
+def get_pgprank(nick):        
+    print ('Player ' + nick + ' requested PGP-scores')
+    for player_stats in list:
+    #tämä ei käy kaikki player_stats objecteja läpi listasta list
+        if nick == player_stats.name:
+            s.say ("{}: PGP:{}".format(player_stats.name, player_stats.pgp))
             break
         else:
             s.say ("{0} not logged in".format(nick))
@@ -82,6 +103,17 @@ def get_rank(nick):
 
 def get_top10():
     list.sort(reverse=True, key=lambda player_stats: player_stats.score)
+    i = 1
+    try:
+        for i in range(10):
+            player_stats = list[i]
+            s.say("#{}:{}: Score:{}".format(i, player_stats.name, player_stats.score))
+    except IndexError:
+        print("Less than 10 players on list")
+
+def get_pgptop10():
+    list.sort(reverse=True, key=lambda player_stats: player_stats.pgp)
+    i = 1
     try:
         for i in range(10):
             player_stats = list[i]
@@ -125,6 +157,12 @@ def on_chat(nick, message):
     if message == '!addplayer':
         print("trying to add player")        
         add_player(nick)
+    
+    if message == '!pgp':
+        get_pgprank(nick)
+    
+    if message == '!pgptop10':
+        get_pgptop10()
 
 
 @s.event
@@ -140,6 +178,8 @@ def on_elim(killer_nick, killer_weapon, victim_nick, victim_weapon):
     for player_stats in list:
         if killer_nick == player_stats.name:
             player_stats.add_kill()
+        if killer_weapon == pgp
+            player_stats.add_pgp()
         if victim_nick == player_stats.name:
             player_stats.add_death()
 
