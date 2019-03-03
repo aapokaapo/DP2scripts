b330def0eccebb4ae884539a1869d4411ecc5f1e
aapokaapo <47606265+aapokaapo@users.noreply.github.com>
2019-03-03 20:37:44 +0200

    Create Clans.py

---
+++ Clans.py
@@ -0,0 +1,40 @@
+# DPLib - Asynchronous bot framework for Digital Paint: Paintball 2 servers
+# Copyright (C) 2017  MichaÅ‚ Rokita
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Ranking system
+
+from dplib.server import Server
+from dplib.dplogin import DPLogin
+
+s = Server(hostname='178.157.90.120', port=22222,
+           logfile=r'/home/paintball/paintball2/pball/qconsole22222.log',
+           rcon_password='endless')
+
+d = DPLogin()
+
+
+clanid_list = []
+
+def add_clan(clanid):
+    members = d.get_clan_members()
+    s.say("")
+
+@s.event
+def on_chat(nick, message):
+    if message.startswith('!addclan'):
+        add_clan(message.replace("!addclan",""))
+        
+s.run()
