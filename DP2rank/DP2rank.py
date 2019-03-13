71d289a6f3746285ecd314c774eb82df36454f48
aapokaapo <47606265+aapokaapo@users.noreply.github.com>
2019-03-14 00:36:32 +0200

    Update DP2rank.py

--- DP2rank/DP2rank.py
+++ DP2rank/DP2rank.py
@@ -1,9 +1,27 @@
+# Copyright (C) 2019  Aapo Kinnunen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from config import s
 from config import infomessage
 import main
 import threading
+import floodprotection as f
+import GRclient
     
-print("ServerSide DP2RankingSystem running. Made by whoa (and Toolwut)\nClan [s] making Digital Paintball2 great again!")
+print("ServerSide DP2RankingSystem running. Made by whoa (and Toolwut)\n------- Clan [s] making Digital Paintball2 great again!-------")
 
 main.leaderboard_load()
 
@@ -15,13 +33,19 @@ def on_chat(nick, message):
         main.get_stats(nick)
 
     if message == '!top10':
-        main.get_top10()
+        flooding = False
+        flooding = f.floodprotection(nick)
+        if not flooding:
+            main.get_top10()
 
     if message == '!addplayer':
         main.add_player(nick)
 
     if message == '!top10kd':
-        main.get_top10kd()
+        flooding = False
+        flooding = f.floodprotection_kd(nick)
+        if not flooding:
+            main.get_top10kd()
         
     if message == '!help':
         main.get_help()
@@ -53,6 +77,9 @@ def on_elim(killer_nick, killer_weapon, victim_nick, victim_weapon):
 @s.event
 def on_mapchange(mapname):
     main.leaderboard_save()
+    GRclient.leaderboard_save()
+    f.on_timeout.clear()
+    f.on_timeout_kd.clear()
 
 t = threading.Timer(300.0, infomessage,())
 t.start()
