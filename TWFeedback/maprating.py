651c2d8338be75251f43e96b55d4d179d8a87bec
g1YC31aA <mobileblender2016@gmail.com>
2019-04-25 19:25:37 +0200

    Not map specific yet but lets only player with Global Login ID vote (once) and replaces further votes, also stores all voters names

--- TWFeedback/maprating.py
+++ TWFeedback/maprating.py
@@ -17,6 +17,7 @@
 # Ranking system
 
 from dplib.server import Server
+from shutil import copyfile
 import os
 import time
 import threading
@@ -27,26 +28,83 @@ s = Server(hostname='127.0.0.1', port=22222,
            
            
 def add_feedback(feedback, nick, good):
+    players=s.get_players()
+    #### Retrieve player's ID ###
+    playerid = 0
+    for player in players:
+        if player.nick == nick:
+            playerid=player.dplogin
+            break
+    
+    ### Define hostname specific file paths ###
     status = s.get_status()
-    mapname=status.get("mapname")
     servername=status.get("hostname")
-    if mapname.startswith("beta"):
-        mapname = mapname.replace("beta/", "")
-    elif mapname.startswith("inprogress"):
-        mapname = mapname.replace("inprogress/", "")
-    filename = "/var/www/html/whoa.ga/mapfeedback-"+servername+".txt"
-
-    if os.path.exists(filename):
-        append_write = 'a' # append if already exists
-    else:
-        append_write = 'w' # make a new file if not
-    with open(filename ,append_write) as myfile:
-        if(good):
-            myfile.write("+ "+nick+": "+feedback + "\n")
+    ratefilename = "/var/www/html/whoa.ga/maprating-"+servername+".txt"    
+    playersfilename = "/var/www/html/whoa.ga/mapplayers-"+servername+".txt" 
+    reasonfilename = "/var/www/html/whoa.ga/mapfeedback-"+servername+".txt"
+    
+    ### Check if player already voted ###
+    f=open(playersfilename, "r")
+    alreadyvoted=false
+    for line in f:
+        if line == playerid:
+            alreadyvoted=true
+    f.close()
+    
+    ### If not, add player's ID to playersfilename and vote+reason to reasonfilename ###
+    if !alreadyvoted:
+        f=open(playersfilename, "a")
+        f.write(playerid)
+        f.close()
+        if os.path.exists(reasonfilename):
+            append_write = 'a' # append if already exists
         else:
-            myfile.write("- "+nick+": "+feedback + "\n")
-        s.say("{C}9***{C}CFeedback written to whoa.ga/mapfeedback-"+servername+".txt{C}9***")
+            append_write = 'w' # make a new file if not
+        with open(reasonfilename ,append_write) as myfile:
+            if(good):
+                myfile.write("+ "+nick+": "+feedback + "\n")
+            else:
+                myfile.write("- "+nick+": "+feedback + "\n")
+    
+    ### If yes, replace player's previous vote+reason with his latest one ###
+    if alreadyvoted:
+        f=open(reasonfilename, "r")
+        for lines in f:
+            line = lines.replace("+ ","")
+            line = line.replace("- ","")
+            if line.startswith(nick):
+                if(good):
+                    replaceline("/var/www/html/whoa.ga/mapfeedback-"+servername, lines, "+ "+nick+": "+feedback + "\n")
+                else:
+                    replaceline("/var/www/html/whoa.ga/mapfeedback-"+servername, lines, "- "+nick+": "+feedback + "\n")
+        f.close()
+        
+    ### Count + and - votes to calculate consensus percentage ###
+    f=open(reasonfilename, "r")
+    pro=0
+    con=0
+    total=0
+    for line in f:
+        total+=1
+        if line.startswith("+"):
+            pro += 1
+        else if line.startswith("-"):
+            con += 1
+    f.close()
+    if pro+con==total:
+        percentage=pro/total
+    s.say("{C}9***{C}CFeedback written to whoa.ga/mapfeedback-"+servername+".txt{C}9***")
    
+def replaceline(path, oldcontent, newcontent):
+    copyfile(path+".txt", path+"tmp.txt")
+    with fdopen(path+".txt", "w") as new_file:
+        with open(path+"tmp.txt") as old_file:
+            for line in old_file:
+                if(line == oldcontent):
+                    new_file.write(newcontent)
+                else:
+                    new_file.write(line)
+    remove(path+"tmp.txt")
 
 @s.event
 def on_chat(nick, message):
