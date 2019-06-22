3e2c72d2983ef786defa2919778e55c0dd4a7652
g1YC31aA <lennartmobil2016@gmail.com>
2019-06-22 19:40:59 +0200

    added .map file reader, adds individual brushes to list and chopping of non-clip-plane lines

---
+++ DP2RadarGen/main.py
@@ -0,0 +1,38 @@
+if __name__ == '__main__':
+    path = "maps/construction_final.map"
+    with open(path, 'r') as file:
+        brushes = list()
+        currentBrush = list()
+        isBrush = False
+        firstBracket = True
+        for line in file:
+            if line.startswith("{"):
+                if firstBracket:
+                    firstBracket = False
+                else:
+                    print("here")
+                    isBrush = True
+            if isBrush and not firstBracket:
+                # print(line)
+                currentBrush.append(line)
+            if line.startswith("}"):
+                print("!")
+                isBrush = False
+                brushes.append(currentBrush)
+                currentBrush = list()
+    print(len(brushes))
+    print(len(brushes[0]))
+    cleanBrushes = list()
+    for i, brush in enumerate(brushes):
+        cleanBrush = list()
+        for idx, line in enumerate(brush[1:-1]):
+            if line.startswith("(") and not "pball/hint" in line and not "pball/clip" in line:
+                print(f"big idx: {i}, small idx: {idx}, line: {line}")
+                cleanBrush.append(line)
+        if not len(cleanBrush)==0:
+            cleanBrushes.append(cleanBrush)
+        cleanBrush = list()
+    for brush in cleanBrushes:
+        print("-----")
+        for line in brush:
+            print(line)
