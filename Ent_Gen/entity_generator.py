015b26895fc45b538be012b3b21656da32559c4d
g1YC31aA <lennartmobil2016@gmail.com>
2019-07-26 10:41:52 +0200

    first full workflow: creates new info_player_deathmatches, extracts old entities out of the .bsp while modifying the worldspawn and writes them to the .ent

--- Ent_Gen/entity_generator.py
+++ Ent_Gen/entity_generator.py
@@ -17,19 +17,77 @@ def get_spawns(path, map):
                 for i in range(5):
                     linez.append(lines[idx+i])
                 ent_lines.append(linez)
+    spawns = list()
+    print(ent_lines)
     for lines in ent_lines:
+        local_spawns = list()
+        local_spawns.append('{')
+        local_spawns.append('"teamnumber" "0"')
+        local_spawns.append('"classname" "info_player_deathmatch"')
+
+        id=lines[1].split(" ")[1].replace("'", "")
+        rotation=round(float(lines[4].split(" ")[1]))+180
+        coordinates = lines[3].split(" ")
+        origin = [round(float(coordinates[i])) for i in range(1,4)]
+        origin = f"{origin[0]} {origin[1]} {origin[2]}"
+        print(f"id: {id}, rotation:{rotation}, origin: {origin}")
+        local_spawns.append(f'"origin" "{origin}"')
+        local_spawns.append(f'"angle" "{rotation}"')
+        local_spawns.append(f'"_loc_id" "{id}"')
+        local_spawns.append('}')
+        spawns.append(local_spawns)
         for line in lines:
             print(line)
         print()
+        print(spawns)
+    return spawns
 
-def get_old_entities(path):
-    print()
 
-def remove_spawns(entities):
-    print()
+def get_old_entities(path, message_attachment):
+    with open(path, "rb") as f:
+        bytes1 = f.read()
+        offset = int.from_bytes(bytes1[8:12], byteorder='little', signed=False)
+        length = int.from_bytes(bytes1[12:16], byteorder='little', signed=False)
+        lines = (bytes1[offset:offset+length-1].decode("ascii", "ignore"))
+        lines = lines.split("\n")
+        print(lines)
+        print(len(lines))
+        ents = list()
+        localents = list()
 
-def worldspawn_to_dm(entities):
-    print()
+        for i in range(len(lines)):
+            localents.append(lines[i])
+            if lines[i] == "}":
+                ents.append(localents)
+                localents = list()
 
-def save_ent(entities, path):
-    print()
+        contains_gamemode = False
+        for idx, line in enumerate(ents[0]):
+            if line.startswith('"message"'):
+                ents[0][idx] = line[0:len(line)-1] + message_attachment + '"'
+            elif line.startswith('"gamemode"'):
+                ents[0][idx] = line[0:12]+'1"'
+                contains_gamemode = True
+            elif line.startswith('"teamnumber"'):
+                ents[0][idx] = ""
+            elif line.startswith('"maxteams"'):
+                ents[0][idx] = ""
+            elif line.startswith('"team'):
+                ents[0][idx] = ""
+        if not contains_gamemode:
+            ents[0].append(ents[0][len(ents[0])-1])
+            ents[0][len(ents[0])-2] = '"gamemode" "1"'
+        ents[0] = [x for x in ents[0] if x]
+        ents = [x for x in ents if not '"classname" "info_player_deathmatch"' in x]
+        print(ents)
+        return ents
+
+
+def save_ent(old_ents, new_ents, path):
+    with open(path, "w") as f:
+        for ent in old_ents:
+            for line in ent:
+                f.write(line+"\n")
+        for ent in new_ents:
+            for line in ent:
+                f.write(line+"\n")
--- Ent_Gen/main.py
+++ Ent_Gen/main.py
@@ -3,10 +3,9 @@ from Ent_Gen import entity_generator as ent_gen
 if __name__ == '__main__':
     log_name = "DM1.txt"
     path = "/home/lennart/paintball2/Paintball2-wine/pball"
-    map = "/beta/duck_fix_b6"
-    mapname = "duck_fix_b6"
+    map = "/maps/hca_fort"
+    mapname = "hca_fort"
+    message_attachment = "\\n- TW-DM Version"
     spawns = ent_gen.get_spawns(path+"/"+log_name, mapname)
-    # old_entities = ent_gen.get_old_entities(map)
-    # new_entities = ent_gen.remove_spawns(old_entities)
-    # new_entities_dm = ent_gen.worldspawn_to_dm(new_entities)
-    # ent_gen.save_ent(new_entities_dm, path+"/maps"+map+".ent")
+    old_entities = ent_gen.get_old_entities(path+map+".bsp", message_attachment)
+    ent_gen.save_ent(old_entities, spawns, path+map+".ent")
